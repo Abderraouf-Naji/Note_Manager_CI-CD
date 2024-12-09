1358bc9c53bc2e73afac32ba32e6ea0ca9c45dde
Abderraouf-Naji <naji.abderraouf@enis.tn>
2024-12-09 12:27:24 +0100

    first commit

---
+++ my-terraform-project/remote-backend/terraform.tfvars
@@ -0,0 +1,6 @@
+# terraform.tfvars
+# Optionally, override the default values with custom unique values
+
+s3_bucket_name     = "custom-terraform-state-bucket-123456" # This should be unique
+dynamodb_table_name = "custom-terraform-state-locks-123456" # This should be unique
+
---
+++ my-terraform-project/remote-backend/variables.tf
@@ -0,0 +1,14 @@
+# Variables for S3 Bucket
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket to store Terraform state"
+  type        = string
+  default     = "enis-terraform-for-state-file-0125"
+}
+
+# Variables for DynamoDB Table
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+  default     = "terraform-state-locking-1256"
+}
+
